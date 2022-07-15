e76273f79bbde01c59b23b15fb28d3f3031d8241
Promethee Toneatti <promethee.toneatti@telecom-sudparis.eu>
2022-07-16 01:07:27 +0200

    debut des fonctions et state du player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    enum States { onFoot, onFly, onWait }
+    States playerState;
+    void Awake()
     {
-        
+        playerState = States.onFoot;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(playerState == States.onWait)
+        {
+            //speed = 0
+        }
+    }
+
+    private void Yeet() //Se mettre en position d'attente du Yeet
+    {
+        if (playerState == States.onFoot)
+        {
+            playerState = States.onWait; //faire en sorte qu'il ne puisse pas se déplacer
+        }
+    }
+
+    public void onYeet(string orientation) //Quand l'autre joueur nous yeet
+    {
+        playerState = States.onFly;
+        //deplacement du joueur selon l'orientation
+        playerState = States.onFoot;
+    }
+
+
+
+    private void OnTriggerEnter2D(Collider2D collision) 
+    {
+        if (playerState == States.onWait && collision.tag == "Player")
+        {
+            Player otherPlayer = collision.GetComponent<Player>();
+            otherPlayer.onYeet("yes");
+            playerState = States.onFoot;
+        }
     }
 }
