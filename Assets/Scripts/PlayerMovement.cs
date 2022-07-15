deb995b05d4561130fc55b417cf1b9a0065a043b
sulray <sulray250@gmail.com>
2022-07-16 01:07:33 +0200

    Player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    private Animator anim;
+    private float inputX;
+    private float inputY;
+    private float directionX;
+    private float directionY;
+    [SerializeField] private float move_speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(inputX , inputY) * move_speed;
+        anim.SetFloat("inputX", inputX);
+        anim.SetFloat("inputY", inputY);
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        inputX = context.ReadValue<Vector2>().x;
+        inputY = context.ReadValue<Vector2>().y;        
+    }
+    
+    public float[] Direction()
+    {
+        return new float[] { inputX, inputY };
+    }
+}
