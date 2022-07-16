ce1169fbb65d8208e1f50c4a55880cb70b0d7934
Promethee Toneatti <promethee.toneatti@telecom-sudparis.eu>
2022-07-16 17:44:15 +0200

    ajout du commit précédent

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,27 +2,89 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
-
+using UnityEngine.InputSystem;
+using System.Linq;
 
 public class Player : MonoBehaviour
-{ 
+{
+    enum States { onFoot, onFly, onWait }
+    States playerState;
+    private CircleCollider2D detection;
+    private PlayerMovement playerMovement;
     [SerializeField] int indexOfPrefab;
     public static event Action<int> ThePlayerSpawns;
+    public Transform playerTransform;
 
-    private void Awake()
+    private float duration = 2f;
+
+    void Awake()
     {
+        playerState = States.onFoot;
+        detection = gameObject.GetComponent<CircleCollider2D>();
+        playerMovement = gameObject.GetComponent<PlayerMovement>();
+        playerTransform = gameObject.GetComponent<Transform>();
     }
 
-    // Start is called before the first frame update
     void Start()
     {
         ThePlayerSpawns?.Invoke(indexOfPrefab);
-
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        print(playerState);
+    }
+
+    public void Yeet(InputAction.CallbackContext context) //Se mettre en position d'attente du Yeet
+    {
+        if (playerState == States.onFoot)
+        {
+            playerState = States.onWait; 
+            playerMovement.setSpeed(0);
+        }
+        if (context.canceled)
+        {
+            playerState = States.onFoot;
+            playerMovement.setSpeed(playerMovement.maxSpeed);
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (playerState == States.onWait && collision.CompareTag("Player"))
+        {
+            float[] direction = playerMovement.getDirection();
+            Player[] players = collision.GetComponents<Player>();
+            Player otherPlayer = players.First(x => x != this);
+            otherPlayer.playerState = States.onFly;
+            otherPlayer.StartCoroutine(otherPlayer.Fly(new Vector2(otherPlayer.playerTransform.position.x, otherPlayer.playerTransform.position.y), 
+                new Vector2(otherPlayer.playerTransform.position.x + direction[0]*5, otherPlayer.playerTransform.position.y+direction[1]*2)));
+            playerState = States.onFoot;
+
+        }
+    }
+
+    public static Vector2 Parabola(Vector2 start, Vector2 end, float height, float t)
+    {
+        Func<float, float> f = x => -4 * height * x * x + 4 * height * x;
+
+        var mid = Vector2.Lerp(start, end, t);
+
+        return new Vector2(mid.x, f(t) + Mathf.Lerp(start.y, end.y, t));
+    }
+
+
+    private IEnumerator Fly(Vector2 start, Vector2 finish)
+    {
+        float animation = 0f;
+        while (animation < duration)
+        {
+            animation += Time.deltaTime;
+            transform.position = Parabola(start, finish, duration, animation / duration);
+            yield return null;
+        }
+        playerState = States.onFoot;
+        yield return null;
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,21 +10,23 @@ public class PlayerMovement : MonoBehaviour
     private Animator anim;
     private float inputX;
     private float inputY;
+    [SerializeField] public float maxSpeed;
     private Vector2 moveDirection;
-    [SerializeField] private float move_speed;
+    private float moveSpeed;
+
 
     // Start is called before the first frame update
     void Start()
     {
         rb = this.GetComponent<Rigidbody2D>();
         anim = this.GetComponent<Animator>();
+        moveSpeed = maxSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        rb.velocity = new Vector2(moveDirection.x , moveDirection.y) * move_speed;
+        rb.velocity = new Vector2(moveDirection.x, moveDirection.y) * moveSpeed;
         anim.SetFloat("inputX", inputX);
         anim.SetFloat("inputY", inputY);
     }
@@ -34,11 +36,17 @@ public class PlayerMovement : MonoBehaviour
         inputX = context.ReadValue<Vector2>().x;
         inputY = context.ReadValue<Vector2>().y;
         moveDirection = new Vector2(inputX, inputY).normalized;
-        
+
     }
-    
-    public float[] Direction()
+
+    public float[] getDirection()
     {
         return new float[] { inputX, inputY };
     }
+
+    public void setSpeed(float value)
+    {
+        moveSpeed = value;
+    }
+
 }
