aa8b528d86e03922e8045c80569bb0903145f2ad
Promethee Toneatti <promethee.toneatti@telecom-sudparis.eu>
2022-07-17 19:41:51 +0200

    cooldown monster

--- Assets/Scripts/Monsters/Monster2.cs
+++ Assets/Scripts/Monsters/Monster2.cs
@@ -7,14 +7,20 @@ public class Monster2 : Monster
     [SerializeField] public GameObject BulletPrefab;
     public override IEnumerator doAttack(Player player)
     {
-        Vector2 directionToTarget = player.transform.position - transform.position;
-        float angle = Vector3.Angle(Vector3.right, directionToTarget);
-        if (player.transform.position.y < transform.position.y) angle *= -1;
-        Quaternion bulletRotation = Quaternion.AngleAxis(angle, Vector3.forward);
-        GameObject bullet = Instantiate(BulletPrefab, transform.position, bulletRotation);
-        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
-        yield return new WaitForSeconds(cooldown);
-        monsterState = MonsterStates.Reaching;
-        yield return null;
+        if (canAttack)
+        {
+            canAttack = false;
+            Vector2 directionToTarget = player.transform.position - transform.position;
+            float angle = Vector3.Angle(Vector3.right, directionToTarget);
+            if (player.transform.position.y < transform.position.y) angle *= -1;
+            Quaternion bulletRotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            GameObject bullet = Instantiate(BulletPrefab, transform.position, bulletRotation);
+            Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+            yield return new WaitForSeconds(cooldown);
+            canAttack = true;
+            monsterState = MonsterStates.Reaching;
+            yield return null;
+        }
+
     }
 }
