9829bbdd204f1c90a813c63eff107d213e55f220
Tom Duval <53614982+BowRaiders@users.noreply.github.com>
2022-07-17 18:49:37 +0200

    Monster prefabs fixed

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -103,8 +103,8 @@ public class Player : MonoBehaviour
 
     public void Roll()
     {
-        currentFace = rnd.Next(0, availablePowers.Count); //Next(int x, int y) returns a value between x and y, upper bound excluded.
-
+        //currentFace = rnd.Next(0, availablePowers.Count); //Next(int x, int y) returns a value between x and y, upper bound excluded.
+        currentFace = 1;
 
         Debug.LogFormat("Cx : {0} rolled {1}", this.gameObject.name, availablePowers[currentFace]);
         currentPower = Power.GetPower(this, availablePowers[currentFace], listPowerPrefabs);
