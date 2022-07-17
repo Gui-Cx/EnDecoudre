f245a0fa6e7ba73cf39fe5918193f1eb200da011
Promethee Toneatti <promethee.toneatti@telecom-sudparis.eu>
2022-07-17 17:04:12 +0200

    aaaaa

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -115,8 +115,8 @@ public class Player : MonoBehaviour
 
     public void Roll()
     {
-        currentFace = rnd.Next(0, availablePowers.Count); //Next(int x, int y) returns a value between x and y, upper bound excluded.
-        //currentFace = (int)PowerEnum.Dash;
+        //currentFace = rnd.Next(0, availablePowers.Count); //Next(int x, int y) returns a value between x and y, upper bound excluded.
+        currentFace = (int)PowerEnum.Machinegun;
         Debug.LogFormat("Cx : {0} rolled {1}", this.gameObject.name, availablePowers[currentFace]);
         currentPower = Power.GetPower(this, availablePowers[currentFace], listPowerPrefabs);
         indexOfFace = currentFace +1;
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -130,7 +130,7 @@ public class PlayerMovement : MonoBehaviour
 
     public float[] getDirection()
     {
-        return new float[] { inputX, inputY };
+        return new float[] {inputXTmp, inputYTmp };
     }
 
     public void setSpeed(float value)
