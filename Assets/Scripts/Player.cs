00449356da1bd5df568d78eefb70b17daa4608cb
Promethee Toneatti <promethee.toneatti@telecom-sudparis.eu>
2022-07-17 19:48:32 +0200

    yes

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -105,7 +105,7 @@ public class Player : MonoBehaviour
     public void Roll()
     {
         currentFace = rnd.Next(0, availablePowers.Count); //Next(int x, int y) returns a value between x and y, upper bound excluded.
-
+        //currentFace = (int)PowerEnum.Machinegun;
 
         Debug.LogFormat("Cx : {0} rolled {1}", this.gameObject.name, availablePowers[currentFace]);
         currentPower = Power.GetPower(this, availablePowers[currentFace], listPowerPrefabs);
